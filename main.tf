624263a761246c0d98681473d751b067bd950572
rmirandaa <41041127+rmirandaa@users.noreply.github.com>
2024-03-26 00:51:17 -0600

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,164 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.41.0"
+    }
+  }
+}
+
+#resource "null_resource" "build_podman_image" {
+  #provisioner "local-exec" {
+   # command = "podman build -f Dockerfile -t my_image"
+  #  #working_dir = "${path.module}/Users/ronmirandaarce/Documents/GitHub/sre-test/terraform_lab_2/Dockerfile"
+ # }
+#}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-2"
+}
+
+resource "aws_instance" "my_iac_lab2" {
+    ami= "ami-0d77c9d87c7e619f9"
+    instance_type = "t2.micro"
+    key_name      = "awskey"
+
+    tags = {
+        "Name" = "My IaC lab2"
+        Origin = "terraform"
+    }
+  }
+
+  resource "null_resource" "podman_pull_and_run" {
+  depends_on = [aws_instance.my_iac_lab2]
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"  # Specify the SSH user for your AMI (ec2-user for Amazon Linux)
+    private_key = file("/Users/ronmirandaarce/Documents/awskey.pem")  # Specify the path to your private key
+    host        = aws_instance.my_iac_lab2.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum -y install podman",  # Install Podman
+      "podman pull docker.io/23ron/image_lab_2:final",  # Pull Docker image from DockerHub
+      "podman run -d -p 5000:5000 b33f5562332a",  # Run container from the pulled image
+      #"curl localhost:5000/store",
+      "curl 127.0.0.1:5000/store",
+    ]
+  }
+}
+
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+locals {
+  cluster_name = "education-eks-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "education-vpc"
+
+  cidr = "10.0.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = 1
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.15.3"
+
+  cluster_name    = local.cluster_name
+  cluster_version = "1.28"
+
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  cluster_endpoint_public_access = true
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
+
+  }
+
+  eks_managed_node_groups = {
+    one = {
+      name = "node-group-1"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+
+    /*two = {
+      name = "node-group-2"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }*/
+  }
+}
+
+
+# https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
+data "aws_iam_policy" "ebs_csi_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+module "irsa-ebs-csi" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "4.7.0"
+
+  create_role                   = true
+  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
+  provider_url                  = module.eks.oidc_provider
+  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+}
+
+resource "aws_eks_addon" "ebs-csi" {
+  cluster_name             = module.eks.cluster_name
+  addon_name               = "aws-ebs-csi-driver"
+  addon_version            = "v1.20.0-eksbuild.1"
+  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+  tags = {
+    "eks_addon" = "ebs-csi"
+    "terraform" = "true"
+  }
+}
+
+
